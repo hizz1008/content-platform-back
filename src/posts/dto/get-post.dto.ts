2aa6ca5d774f1e523d1052fb327a465c6c9037d4
Junyeon <jun981008@naver.com>
2025-06-12 19:51:26 +0900

    refactor: 프로젝트 구조를 간소화하기 위해 posts 모듈과 관련 파일들을 삭제했습니다.

--- src/posts/dto/get-post.dto.ts
+++
@@ -1,6 +0,0 @@
-export class GetPostDto {
-  id: number;
-  title: string;
-  content: string;
-  authorId: number;
-}
--- src/posts/dto/update-post.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostDto } from './create-post.dto';
-
-export class UpdatePostDto extends PartialType(CreatePostDto) {
-  id: number;
-  title: string;
-  content: string;
-  authorId: number;
-}
