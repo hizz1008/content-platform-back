e5f8440be6f2302ccc1f7d4a9544e32e9e8aaf86
peter <jun981008@gmail.com>
2025-07-07 23:42:01 +0900

    feat: JWT 인증 기능 추가 - refreshToken을 사용하여 JWT를 검증하고, 쿠키에 refreshToken을 저장하는 기능을 구현했습니다. AuthController에 refresh 메서드를 추가하여 인증 흐름을 개선했습니다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,15 @@ import { Controller, Post, UseGuards, Request, Res } from '@nestjs/common';
 import { LocalAuthGuard } from './guard/auth.guard';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req, @Res({ passthrough: true }) res: Response) {
@@ -19,5 +24,17 @@ export class AuthController {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7일
     });
     return { accessToken: tokens.access_token };
+  }
+  @Post('refresh')
+  @UseGuards(JwtAuthGuard)
+  async refresh(@Request() req, @Res({ passthrough: true }) res: Response) {
+    const refreshToken = req.cookies.refreshToken;
+    // const tokens = await this.authService. (refreshToken);
+    const tokens = this.jwtService.verify(refreshToken);
+
+    res.cookie('refreshToken', tokens.refresh_token, {
+      httpOnly: true,
+      sameSite: 'strict',
+    });
   }
 }
